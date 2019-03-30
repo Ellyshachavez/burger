74438a00bf1f3fb7633479a05bee47014757e9fb
Ellysha Chavez <ellyshachavez@gmail.com>
2019-03-30 14:51:50 -0700

    files & database add

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+### Schema
+DROP DATABASE IF EXISTS burgers_db
+
+CREATE DATABASE burgers_db
+
+use burgers_db
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT,
+    burger_name VARCHAR(49) NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ server.js
@@ -0,0 +1,5 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
