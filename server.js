1621e0ebc5879fe4640d9134f1f4113e4329248b
Ellysha Chavez <ellyshachavez@gmail.com>
2019-04-01 23:33:32 -0700

    port

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "zackylove4649",
+  database: "burgers_db"
+});
