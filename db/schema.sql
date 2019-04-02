ad8219e2f73db5e0cf7af05a0786aa2ed6370ba1
Ellysha Chavez <ellyshachavez@gmail.com>
2019-04-01 23:31:13 -0700

    seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,15 @@
 ### Schema
-DROP DATABASE IF EXISTS burgers_db
+DROP DATABASE IF EXISTS burgers_db;
 
-CREATE DATABASE burgers_db
+CREATE DATABASE burgers_db;
 
-use burgers_db
+use burgers_db;
 
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT,
     burger_name VARCHAR(49) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT false,
     PRIMARY KEY (id)
-);
+); 
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Inserted a min of 3 records into the table
+
+INSERT INTO burgers (burger_name) VALUES ("Texas Chainsaw Massa-Curd Burger");
+INSERT INTO burgers (burger_name) VALUES ("The Final Kraut Down Burger");
+INSERT INTO burgers (burger_name) VALUES ("She’s a Super Leek Burger");
+INSERT INTO burgers (burger_name) VALUES ("Let’s Give ‘Em Something Shiitake ‘Bout Burger");
+INSERT INTO burgers (burger_name) VALUES ("Itsy Bitsy Teeny Weenie Yellow Polka Dot Zucchini Burger");
+
