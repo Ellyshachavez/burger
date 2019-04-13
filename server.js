7137f0296558d3664b5104e7a11cab9179e5b539
Ellysha Chavez <ellyshachavez@gmail.com>
2019-04-12 23:09:00 -0700

    server.js

--- server.js
+++ server.js
@@ -14,3 +14,34 @@ var connection = mysql.createConnection({
   password: "zackylove4649",
   database: "burgers_db"
 });
+
+
+// parse app
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Static directory to be serve
+app.use(express.static(__dirname + '/public'));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+var routes = require("./controllers/burgers_controller");
+app.use('/', routes);
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+});
+
+// Start server
+app.listen(PORT, function () {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
+});
